e008290589f63b5043ca0f3186b4ae5040852d77
Joe DeCapo <joe@polka.cat>
2017-11-21 21:35:35 -0600

    Update dendencies

--- Sources/App/Controllers/TravisController.swift
+++ Sources/App/Controllers/TravisController.swift
@@ -3,6 +3,7 @@ import Foundation
 func runCoverageScript(buildNumber: String, commit: String) {
     let task = Process()
     let currentPath = FileManager.default.currentDirectoryPath
+    task.environment = ProcessInfo.processInfo.environment
     task.launchPath = "\(currentPath)/script/clowder-coverage.sh"
     task.arguments = [buildNumber, commit]
     task.launch()
